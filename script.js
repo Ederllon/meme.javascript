e333ebe9236713924385484c05256d21cf63e775
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-06 20:03:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const gerarMemeBtn = document.getElementById('gerarMemeBtn');
+    const memeImg = document.getElementById('memeImg');
+
+    gerarMemeBtn.addEventListener('click', function() {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(data => {
+                const memes = data.data.memes;
+                const meme = memes[Math.floor(Math.random() * memes.length)];
+                memeImg.src = meme.url;
+            })
+            .catch(error => console.error('Erro ao obter memes:', error));
+    });
+});
